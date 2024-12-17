081f93c9c29243fdf9f601a3628a143d72f04a73
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-12-17 18:39:49 +0530

    Create Popular component to display popular courses
    Add dummy courses and related assets

--- frontend/src/components/Popular/Popular.tsx
+++ frontend/src/components/Popular/Popular.tsx
@@ -1,9 +1,25 @@
 import styles from "./Popular.module.css";
+import popular_courses from "../../assets/popular_courses";
+import CourseCard from "../CourseCard/CourseCard";
 
 const Popular = () => {
   return (
     <section className={styles.popular}>
       <h2>Most Popular Courses</h2>
+      <hr />
+
+      <div className={styles.popularCourse}>
+        {popular_courses.map((course) => {
+          return (
+            <CourseCard
+              id={course.id}
+              name={course.name}
+              tags={course.tags}
+              image={course.image}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 };
