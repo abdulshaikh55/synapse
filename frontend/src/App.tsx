2c39c172cae95d0a6617479b047b588e0bed38f1
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-12-19 21:21:24 +0530

    Add: routes to navigate to main and signupLogin page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 import Main from "./pages/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignupLogin from "./pages/SignupLogin/SignupLogin";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Main />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signupLogin" element={<SignupLogin />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
