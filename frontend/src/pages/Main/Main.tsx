ed674cb4457bc65f9eb5d7653fd32504590fe989
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2024-12-25 21:35:23 +0530

    Update Main page: add resources section, add about test section

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -2,7 +2,9 @@ import styles from "./Main.module.css";
 import banner from "../../assets/banners/banner.png";
 import Survey from "../../components/Survey/Survey";
 import Popular from "../../components/Popular/Popular";
-import AboutTests from "../../components/AboutTests/AboutTests";
+import yami_quote from "../../assets/yami_quote.png";
+import resource_in from "../../assets/coding_banner.jpg";
+import resource_out from "../../assets/contribute_banner.jpg";
 
 const Hero = () => {
   return (
@@ -14,7 +16,53 @@ const Hero = () => {
       </div>
 
       <Popular />
-      <AboutTests />
+      <section className={styles.aboutTests}>
+        <div className={styles.left}>
+          <h2>Test your Limits</h2>
+
+          <div className={styles.skillSection}>
+            <h3>Skill Assessment</h3>
+            <p>
+              Take tests of the courses you want to / have completed and assess
+              your level
+            </p>
+          </div>
+
+          <div className={styles.skillSection}>
+            <h3>Coding Practice</h3>
+            <p>Level up your coding skills by practicing problem solving</p>
+          </div>
+
+          <div className={styles.skillSection}>
+            <h3>Inbuilt IDE</h3>
+            <p>
+              Directly access Integrated Development Environment built into the
+              website for the most popular programming languages
+            </p>
+          </div>
+
+          <div></div>
+        </div>
+        <div className={styles.right}>
+          <img src={yami_quote} alt="Quote" />
+        </div>
+      </section>
+
+      <section className={styles.additionalResources}>
+        <h2>Additional Resources</h2>
+        <div className={styles.io}>
+          <div className={styles.in}>
+            <img src={resource_in} alt="Coding logo" />
+            <h3>Alumni resources</h3>
+            <p>Take a look at these resources compiled by your alumni</p>
+          </div>
+          <div className={styles.out}>
+            <img src={resource_out} alt="Contribution banner" />
+            <h3>Contribute</h3>
+            <p>Share the resources you have found useful with your juniors</p>
+          </div>
+        </div>
+      </section>
     </section>
   );
 };
