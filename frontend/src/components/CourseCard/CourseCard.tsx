1898d7ed6b9923b3c5d63e9ba4117576163d35c7
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2025-02-09 00:02:39 +0530

    refactor(CourseCard): Improve styling and add semantic class names

--- frontend/src/components/CourseCard/CourseCard.tsx
+++ frontend/src/components/CourseCard/CourseCard.tsx
@@ -4,14 +4,19 @@ const CourseCard = (props: any) => {
   return (
     <div className={styles.courseCard}>
       <a href={props.url}>
-        <img src={props.image} alt={props.name} />
-
+        <img
+          src={props.image}
+          alt={props.name}
+          className={styles.courseCardImage}
+        />
         <h3>{props.name}</h3>
       </a>
 
-      <ul>
+      <ul className={styles.courseCardList}>
         {props.tags.slice(0, 3).map((tag: string, index: number) => (
-          <li key={index}>#{tag}</li>
+          <li key={index} className={styles.courseCardListItem}>
+            #{tag}
+          </li>
         ))}
       </ul>
     </div>
