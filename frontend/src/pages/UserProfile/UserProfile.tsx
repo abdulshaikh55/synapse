339587a4dfbc6f7d652cfce039113f6ec1f59686
abdulshaikh55 <116955072+abdulshaikh55@users.noreply.github.com>
2025-02-16 20:42:05 +0530

    update(UserProfile): add profile pic, show enrolled courses

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,24 +1,46 @@
 import styles from "./UserProfile.module.css";
+import user from "./user_profile";
+import courses from "../../assets/courses";
+import CourseCard from "../../components/CourseCard/CourseCard";
 
 const UserProfile = () => {
+  const userCourses = user.courses_enrolled;
   return (
     <div className={styles.userProfile}>
-      <h1 className={styles.title}>👤 User Profile</h1>
-      <div className={styles.profileInfo}>
-        <img
-          src="/path/to/profile-pic.jpg"
-          alt="Profile"
-          className={styles.profilePic}
-        />
-        <div className={styles.details}>
-          <h2 className={styles.username}>Username: JohnDoe</h2>
-          <p className={styles.email}>Email: johndoe@example.com</p>
-          <p className={styles.bio}>
-            Bio: A passionate developer and tech enthusiast! 🚀
-          </p>
+      <div className={styles.mainInfo}>
+        <div className={styles.profilePic}>
+          <img src={user.profile_picture} alt="Profile picture" />
+        </div>
+
+        <div className={styles.identification}>
+          <div className={styles.username}>
+            <h1>{user.username}</h1>
+          </div>
+          <div className={styles.email}>
+            <h2>{user.email}</h2>
+          </div>
+          <div className={styles.id}>
+            <h2>ID: {user.id}</h2>
+          </div>
         </div>
       </div>
-      <button className={styles.editButton}>✏️ Edit Profile</button>
+
+      <div className={styles.courseInfo}>
+        <h2>Courses Enrolled</h2>
+        <div className={styles.coursesContainer}>
+          {userCourses.map((courseId) => {
+            const course = courses.find((c) => c.id === courseId);
+            return course ? (
+              <CourseCard
+                key={course.id}
+                {...course}
+                className={styles.courseCard}
+              />
+            ) : null;
+          })}
+        </div>
+      </div>
+      <button className={styles.editButton}>Edit Profile</button>
     </div>
   );
 };
---
+++ frontend/src/pages/UserProfile/user_profile.ts
@@ -0,0 +1,10 @@
+import profile_pic from "../../assets/hinata_profile_pic.jpg"
+export const user_profile = {
+  id: "10001",
+  username: "Hinata",
+  email: "hinata8@hyugaclan.com",
+  profile_picture: profile_pic,
+  courses_enrolled: [2, 7, 4],
+}
+
+export default user_profile;
